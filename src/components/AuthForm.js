dc4dcf9f2d0e7feaddb3c538bd2a7819693aa482
vika <ms.victoryart@gmail.com>
2023-08-15 14:01:59 +0300

    добавила обработчик, который отслеживает изменение инпутов, а также необходимые стейт-переменные для компонента AuthForm, в работе — защита роута "/" с помощью НОС

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function AuthForm(props) {
+  const [userEmail, setUserEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleChange(e) {
+    const {name, value} = e.target; //используем деструктуризацию объекта, чтобы упростить код
+
+    if (name === "userEmail") {
+      setUserEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    //здесь должна быть функциональность сабмита
+  }
+
   return (
     <div className="authform">
       <p className="authform__title">{props.title}</p>
-      <form className="authform__form">
+      <form className="authform__form" onSubmit={handleSubmit}>
         <input
+          name="userEmail"
           type="email"
           className="authform__text"
           placeholder="Email"
+          value={userEmail}
+          onChange={handleChange}
         ></input>
         <input
+          name="password"
           type="password"
           className="authform__text"
           placeholder="Пароль"
+          value={password}
+          onChange={handleChange}
         ></input>
         <button type="submit" className="authform__link">
           {props.buttonText}
